78561b1949e04219f03a47632013f57d7f4c2221
Germallet <germanmallet@gmail.com>
2020-04-04 19:52:27 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 const app = express();
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
