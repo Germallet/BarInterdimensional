e88c28da967a5dbd4f7d3ca665455b991150d609
Germallet <germanmallet@gmail.com>
2020-04-04 19:55:18 -0300

    Add files via upload

---
+++ src/inicio.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 80;
+const app = express();
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
