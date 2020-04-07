761971f067cd70798f906a38a2f1863503e5f9b7
Sebas <cabral.K00@gmail.com>
2020-04-07 15:05:24 -0300

    s

--- src/BotDiscord.ts
+++ src/BotDiscord.ts
@@ -1,18 +1,32 @@
 import * as discord from "discord.js";
 
-export class BotDiscord {
+export class BotDiscord 
+{
 	private readonly cliente: discord.Client = new discord.Client();
 
-	public Conectarse(): void {
+	public Conectarse(): void 
+	{
 		this.cliente.on('ready', this.OnReady);
 		this.cliente.on('message', this.OnMessage);
-		this.cliente.login(process.env.DISCORD_BOT_TOKEN);
+		this.cliente.login(process.env.DISCORD_BOT_TOKEN).then(()=>this.CrearCanal());
 	}
 
-	private OnReady() {
+	private OnReady() 
+	{
 		console.log('Conectado!');
 	}
-	private async OnMessage(message: discord.Message, client: discord.Client) {
-		console.log(`Nuevo mensaje: ${message.content}`);
+	private async OnMessage(message: discord.Message, client: discord.Client) 
+	{
+		console.log(`Nuevo mensaje: ${message.content}`);	
+		console.log(`Nuevo mensaje: ${message.guild.id}`);
+		
+	}
+
+	private CrearCanal()
+	{
+		this.cliente.guilds.get('696105418065313934').createChannel('Canal_De_Prueba', 'text')
+		.then(console.log)
+		.catch(console.error);
+		console.log('Canal Creado');
 	}
 }
---
+++ src/Casilla.ts
@@ -0,0 +1,13 @@
+import * as discord from "discord.js";
+
+export class Casilla
+{
+    private rol : discord.Role;
+    private canalVoz : discord.Channel;
+    private canalTexto : discord.Channel;
+    
+    public Crear()
+    {
+        
+    }
+}
