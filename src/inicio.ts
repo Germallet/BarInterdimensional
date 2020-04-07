aaed06da8df2ed9962287a27d78670614545df89
Sebas <cabral.K00@gmail.com>
2020-04-07 13:51:42 -0300

    El principio del Fin

--- src/inicio.ts
+++ src/inicio.ts
@@ -1,10 +1,4 @@
-//import express from 'express';
 import { BotDiscord } from './BotDiscord';
-
-const PORT = process.env.PORT || 80;
-/*const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));*/
 
 const botDiscord = new BotDiscord();
 botDiscord.Conectarse();
