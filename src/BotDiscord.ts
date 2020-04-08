2357877807c912786b25e18a524897e353a15731
Sebas <cabral.K00@gmail.com>
2020-04-08 01:22:06 -0300

    El Bot Reconoce cuando alguien Entra/Sale/Cambia de canal

--- src/BotDiscord.ts
+++ src/BotDiscord.ts
@@ -10,6 +10,26 @@ export class BotDiscord
 	{
 		this.cliente.on('ready', this.OnReady);
 		this.cliente.on('message', this.OnMessage);
+
+		this.cliente.on('voiceStateUpdate', (miembroViejo, miembroNuevo) => 
+		{
+			let canalViejo = miembroViejo.voiceChannel;
+			let canalNuevo = miembroNuevo.voiceChannel;
+		  
+			if(canalViejo !== canalNuevo) 
+			{
+				if(canalViejo === undefined)
+				Consola.Normal('[DISCORD]', `Vale por el rol del canal ${canalNuevo}`); //Agregar rol de casilla de canal nuevo
+
+				else if(canalNuevo === undefined)
+				Consola.Normal('[DISCORD]', `Te sacamos el rol del ${canalViejo}`); //Quitar rol de casilla de canal viejo
+
+				else
+				Consola.Normal('[DISCORD]', `Te cambiamos el rol del ${canalViejo} por el del ${canalNuevo}`);//Quitar rol de casilla de canal viejo y Agregar rol de casilla de canal nuevo
+		  
+			}
+		})
+
 		await this.cliente.login(process.env.DISCORD_BOT_TOKEN); // PROMESA
 		this.CrearMundos();
 	}
