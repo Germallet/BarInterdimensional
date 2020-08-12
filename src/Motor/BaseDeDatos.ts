360a743842ce4e3a37579f53de5667199a92e335
Germán Mallet <germanmallet@gmail.com>
2020-08-11 21:58:43 -0300

    Mundos BD

--- src/Motor/BaseDeDatos.ts
+++ src/Motor/BaseDeDatos.ts
@@ -1,13 +1,11 @@
-/*import { PrismaClient, UserDelegate, User } from "@prisma/client"
-import { Consola } from './Consola';
+import * as BD from "@prisma/client"
  
 export class BaseDeDatos {
 
-    private prisma: PrismaClient = new PrismaClient()
+    private prisma: BD.PrismaClient = new BD.PrismaClient()
 
-    public async main() {
-        const tablaUser: UserDelegate = await this.prisma.user;
-        const usuario1: User = (await tablaUser.findMany())[1];
-        console.log(usuario1.email)
+    public async ObtenerMundos(): Promise<BD.Mundo[]> {
+        const tablaMundo: BD.MundoDelegate = this.prisma.mundo;
+        return tablaMundo.findMany();
     }
-}*/
+}
